6981266cf9579d78ba974aaac82d2fb717373791
jennifer-caroline <jennifer.9586859@aluno.mg.gov.br>
2026-04-01 16:10:18 +0000

    alteração

--- js/script.js
+++ js/script.js
@@ -1,30 +1,19 @@
-let likeCount = 0;
-let curtido = false; // flag booleana
+    let likeCount = 0;
+    let curtido = false; // flag booleana
 
-function curtir() {
+    function curtir() {
 
- if(curtido == false){
-    likeCount++;
-    curtido = true;
-    document.getElementById("likeCount").innerText = likeCount;
- }else{
-    likeCount--;
-    curtido = false;
-    document.getElementById("likeCount").innerText = likeCount;
- }
-
-  
-}
-
-document.getElementById("likeBtn").addEventListener("click", curtir);
-
-
-function descurtir() {
-    if (likeCount > 0) {
-        likeCount--;
+      if(curtido == false){
+        likeCount++;
+        curtido = true;
         document.getElementById("likeCount").innerText = likeCount;
-    }
-}
+      }else{
+        likeCount--;
+        curtido = false;
+        document.getElementById("likeCount").innerText = likeCount;
+      }
 
-document.getElementById("likeBtn").addEventListener("click", curtir);
-document.getElementById("dislikeBtn").addEventListener("click", descurtir);
+    }
+
+    document.getElementById("likeBtn").addEventListener("click", curtir);
+ 
